6b0204a0ebd45ec6c22b8729624ea8715bd84cd4
rcormie <r.cormie@gmail.com>
2023-07-04 17:14:18 -0700

    Updated server.js

--- app/server.js
+++ app/server.js
@@ -1,19 +1,20 @@
+// server.js
+
 // Import the built-in http module from Node.js. This module allows you to 
 // create a web server that can listen for HTTP requests.
 const http = require('http');
 
-// Import the function runPuppeteer from index.js. This function 
-// will launch a new Puppeteer browser, navigate to a URL, take a screenshot, 
-// and then close the browser.
-const runPuppeteer = require('./index');
+// Import the function launchBrowser from puppeteerUtils.js. This function 
+// will launch a new Puppeteer browser in the background.
+const { launchBrowser } = require('./utils');
 
 // Create a new HTTP server. The function passed to createServer is called 
 // every time a client makes a request to the server. The req argument 
 // represents the request from the client, while the res argument is used to 
 // send a response back to the client.
 http.createServer((req, res) => {
-  // Write a response body of 'Hello, world!'
-  res.write('Hello, world!');
+  // Write a response body that the server has been initialized
+  res.write('Node.js server initialized.');
   
   // End the response. If you don't call res.end(), the client will keep 
   // waiting for the rest of the response.
@@ -26,6 +27,6 @@ http.createServer((req, res) => {
   console.log('Server started at http://localhost:8080');
 });
 
-// Run the Puppeteer script. If there's an error while running the script, 
+// Launch the Puppeteer browser in the background. If there's an error while launching the browser, 
 // catch it and log it to the console.
-runPuppeteer().catch(console.error);
+launchBrowser().catch(console.error);
